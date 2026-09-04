5346cce73af7d82a8d13caf7f850e6737fd48363
yukigaya <kyoucha12@gmail.com>
2026-09-04 17:51:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -53,8 +53,7 @@ let collectedStamps =
 const stampList =
     document.getElementById("stamp-list");
 
-const resetButton =
-    document.getElementById("reset-button");
+
 
 const scanQrButton =
     document.getElementById("scan-qr-button");
@@ -372,29 +371,7 @@ function stopQrScanner() {
 // リセット
 // =========================
 
-resetButton.addEventListener(
-    "click",
-    function() {
 
-        collectedStamps = [];
-
-
-        // localStorageから削除
-        localStorage.removeItem(
-            "collectedStamps"
-        );
-
-
-        // 画面を更新
-        displayStamps();
-
-
-        showMessage(
-            "スタンプをリセットしました。"
-        );
-
-    }
-);
 // =========================
 // URLからスタンプIDを取得
 // =========================
